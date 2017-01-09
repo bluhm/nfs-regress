e3e2669cc4d17f27539ef4e648cbd634a58f738a
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-01-09 21:49:06 +0100

    Read tcpstat from kernel into nfs mapped file.

--- mmap-sysctl.c
+++ mmap-sysctl.c
@@ -1,6 +1,7 @@
 #include <sys/mman.h>
 #include <sys/queue.h>
 #include <sys/socket.h>
+#include <sys/sysctl.h>
 
 #include <netinet/in.h>
 #include <netinet/tcp.h>
@@ -15,16 +16,20 @@
 int
 main(void)
 {
-	char file[] = "/nfs/nfsfile";
-	int fd;
-	char *p;
+	char *p, file[] = "/nfs/nfsfile";
+	size_t len;
+	int fd, mib[] = { CTL_NET, PF_INET, IPPROTO_TCP, TCPCTL_STATS };
 
 	if ((fd = open(file, O_WRONLY|O_CREAT, 0777)) == -1)
 		err(1, "open '%s'", file);
 
 	p = mmap(NULL, sizeof(struct tcpstat), PROT_READ, MAP_SHARED, fd, 0);
 	if (p == NULL)
-		err(1, "mmap '%s'", file);
+		err(1, "mmap");
+
+	len = sizeof(struct tcpstat);
+	if (sysctl(mib, 4, p, &len, NULL, 0) == -1)
+		err(1, "sysctl tcpstat");
 
 	return (0);
 }
