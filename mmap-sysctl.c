c4b530e2b50ba9177030a53db888afecd57d4fcd
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-01-09 23:35:02 +0100

    Write sysctl keepalive to trigger a read from nfs.

--- mmap-sysctl.c
+++ mmap-sysctl.c
@@ -21,8 +21,8 @@ int
 main(void)
 {
 	char *p, file[] = "/tmp/nfsfile", page[PAGE_SIZE];
-	size_t len;
-	int fd, mib[] = { CTL_NET, PF_INET, IPPROTO_TCP, TCPCTL_STATS };
+	int fd;
+	int mib[] = { CTL_NET, PF_INET, IPPROTO_TCP, TCPCTL_ALWAYS_KEEPALIVE };
 
 	if ((fd = open(file, O_WRONLY|O_CREAT|O_TRUNC, 0777)) == -1)
 		err(1, "open write '%s'", file);
@@ -33,16 +33,16 @@ main(void)
 		err(1, "close write");
 
 	if ((fd = open(file, O_RDWR)) == -1)
-		err(1, "open read '%s'", file);
+		err(1, "open mmap '%s'", file);
 	p = mmap(NULL, sizeof(struct tcpstat), PROT_READ|PROT_WRITE,
 	    MAP_SHARED, fd, 0);
 	if (p == MAP_FAILED)
 		err(1, "mmap");
-	len = sizeof(struct tcpstat);
-	if (sysctl(mib, sizeof(mib) / sizeof(mib[0]), p, &len, NULL, 0) == -1)
-		err(1, "sysctl tcpstat");
+	if (sysctl(mib, sizeof(mib) / sizeof(mib[0]), NULL, 0,
+	    p, sizeof(int)) == -1)
+		err(1, "sysctl keepalive");
 	if (close(fd) == -1)
-		err(1, "close read");
+		err(1, "close mmap");
 
 	return (0);
 }
